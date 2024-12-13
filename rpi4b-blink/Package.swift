92b775c1167c2cf0323cd7e5c6e4628ed69d4567
CMD <80255379+iCMDdev@users.noreply.github.com>
2024-12-13 23:58:33 +0200

    Fix styling

--- rpi4b-blink/Package.swift
+++ rpi4b-blink/Package.swift
@@ -4,33 +4,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "RPi4B-Blink",
-    platforms: [
-    .macOS(.v14),
+  name: "RPi4B-Blink",
+  platforms: [
+    .macOS(.v14)
   ],
-    products: [
-        .library(
-            name: "MainApp",
-            type: .static,
-            targets: ["MainApp"]),
-    ],
-    dependencies: [
-        .package(
-        url: "https://github.com/apple/swift-mmio.git",
-        branch: "swift-embedded-examples"),
-    ],
-    targets: [
-        .target(
-            name: "MainApp",
-            dependencies: [
-                .product(name: "MMIO", package: "swift-mmio")
-            ],
-            swiftSettings: [
-                .enableExperimentalFeature("Embedded"),
-                .unsafeFlags(["-Xfrontend", "-function-sections"])
-            ]
-        ),
-        .target(name: "Support")
-            
-    ]
+  products: [
+    .library(
+      name: "MainApp",
+      type: .static,
+      targets: ["MainApp"])
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/apple/swift-mmio.git",
+      branch: "swift-embedded-examples")
+  ],
+  targets: [
+    .target(
+      name: "MainApp",
+      dependencies: [
+        .product(name: "MMIO", package: "swift-mmio")
+      ],
+      swiftSettings: [
+        .enableExperimentalFeature("Embedded"),
+        .unsafeFlags(["-Xfrontend", "-function-sections"]),
+      ]
+    ),
+    .target(name: "Support"),
+
+  ]
 )
--- rpi5-blink/Sources/MainApp/MainApp.swift
+++ rpi5-blink/Sources/MainApp/MainApp.swift
@@ -2,58 +2,55 @@ import MMIO
 
 @Register(bitWidth: 32)
 struct GIOIODIR {
-    @ReadWrite(bits: 9..<10, as: Bool.self)
-    var direction: DIRECTION
+  @ReadWrite(bits: 9..<10, as: Bool.self)
+  var direction: DIRECTION
 }
 
 @Register(bitWidth: 32)
 struct GIODATA {
-    @ReadWrite(bits: 9..<10, as: Bool.self)
-    var value: VALUE
+  @ReadWrite(bits: 9..<10, as: Bool.self)
+  var value: VALUE
 }
 
 @RegisterBlock
 struct GPIO {
-    @RegisterBlock(offset: 0x00008)
-    var gioiodir: Register<GIOIODIR>
-    @RegisterBlock(offset: 0x00004)
-    var giodata: Register<GIODATA>
+  @RegisterBlock(offset: 0x00008)
+  var gioiodir: Register<GIOIODIR>
+  @RegisterBlock(offset: 0x00004)
+  var giodata: Register<GIODATA>
 }
 
-
-let gpio = GPIO(unsafeAddress: 0x107d517c00)
-
+let gpio = GPIO(unsafeAddress: 0x10_7d51_7c00)
 
 func setLedOutput() {
-    gpio.gioiodir.modify {
-        $0.direction = false // 0 is output, 1 is input
-    }
+  gpio.gioiodir.modify {
+    $0.direction = false  // 0 is output, 1 is input
+  }
 }
 
 func ledOn() {
-    gpio.giodata.modify {
-        $0.value = true  // pin on
-    }
+  gpio.giodata.modify {
+    $0.value = true  // pin on
+  }
 }
 
 func ledOff() {
-    gpio.giodata.modify {
-        $0.value = false // pin off
-    }
+  gpio.giodata.modify {
+    $0.value = false  // pin off
+  }
 }
-
 
 @main
 struct Main {
 
-    static func main() {
-        setLedOutput()
+  static func main() {
+    setLedOutput()
 
-        while true {  
-            ledOn()
-            for _ in 1..<100000 { } // just a delay
-            ledOff()
-            for _ in 1..<100000 { } // just a delay
-        }
+    while true {
+      ledOn()
+      for _ in 1..<100000 {}  // just a delay
+      ledOff()
+      for _ in 1..<100000 {}  // just a delay
     }
+  }
 }
